e13d1d2a3b930364261e5105586394923f4b1f6f
VisionSystem31 <150258390+VisionSystem31@users.noreply.github.com>
2023-11-16 17:11:54 -0400

    Sistema_01

---
+++ test.py
@@ -0,0 +1,26 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('Renal_Model_S1.pt')
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+
+while cap.isOpened():
+
+    success, frame = cap.read()
+
+    if success:
+        results = model(frame, device=0, imgsz=640, conf=0.25)
+
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
