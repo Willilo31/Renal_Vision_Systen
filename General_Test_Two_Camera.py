9cd157b569a623ba3337ed8c4f51ff1f0dd6bfc6
VisionSystem31 <vsistema31@gmail.com>
2024-05-14 11:41:43 -0400

    funciona

--- General_Test_Two_Camera.py
+++ General_Test_Two_Camera.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-cap2 = cv2.VideoCapture(4)
+cap2 = cv2.VideoCapture(2)
 cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
